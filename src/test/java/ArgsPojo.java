ad26175f27af008e50516bf457f02fbb941a184c
Dom <andreu_semin@mail.ru>
2024-07-29 23:27:08 +0300

    Дз 17

---
+++ src/test/java/ArgsPojo.java
@@ -0,0 +1,21 @@
+import io.restassured.response.ExtractableResponse;
+
+public class ArgsPojo {
+    private String foo1;
+    private String foo2;
+
+    public ArgsPojo(String foo1, String foo2) {
+        this.foo1 = foo1;
+        this.foo2 = foo2;
+    }
+
+    public String getFoo1() {
+        return foo1;
+    }
+
+    public String getFoo2() {
+        return foo2;
+    }
+
+
+}
